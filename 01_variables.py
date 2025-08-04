041c6dc07532c253d70581a7e96d1870137e81ba
Noriel Achero <acheronorielr@gmail.com>
2025-08-04 17:26:08 +0800

    asdasd

---
+++ 01_variables.py
@@ -0,0 +1,33 @@
+# Strings
+
+first_name = "Noriel"
+food = "pizza" 
+email = "noriel@gmail.com"
+
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
+
+# Integers
+
+age = 25
+quantity = 3
+
+print(f"Your age is {age}")
+print(f"You are buying {quantity}")
+
+# Float
+
+price = 10.99
+
+print(f"The price is ${price}")
+
+is_student = True
+is_employee = False
+
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
