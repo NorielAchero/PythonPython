0a9e50e79f014f8c168c7e1916ad7012fd136863
Noriel Achero <acheronorielr@gmail.com>
2025-08-04 17:46:28 +0800

    234234

---
+++ 03_input.py
@@ -0,0 +1,7 @@
+name = input("What is your name: ")
+age = int(input("What is your age: "))
+
+age = age + 1
+
+print(f"Hello, {name}")
+print(f"Your age is {age}")
---
+++ ex1.py
@@ -0,0 +1,8 @@
+# Calculate the Area of a Rectagle
+
+width = float(input("Enter the value of Width: "))
+length = float(input("Enter the value of Length: "))
+
+area = width * length
+
+print(f"The area of the rectangle is {area}")
