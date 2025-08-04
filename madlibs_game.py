53079ccc4fb029a38372bfe5bccd3cd4ac12a56a
Noriel Achero <acheronorielr@gmail.com>
2025-08-04 17:57:02 +0800

    asdasd

---
+++ madlibs_game.py
@@ -0,0 +1,12 @@
+
+
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+adjective2 = input("Enter an adjective: ")
+adjective3 = input("Enter an adjective: ")
+verb1 = input("Enter a verb: ")
+
+print(f"Today I went to a {adjective1} zoo")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective1} and {verb1}")
+print(f"I was {adjective3}!")
