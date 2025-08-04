6d363879bcd9820a29d5fe6b3bd049517e1bdc48
Noriel Achero <acheronorielr@gmail.com>
2025-08-04 17:39:19 +0800

    sdfsd

---
+++ 02_typeCasting.py
@@ -0,0 +1,33 @@
+# Type Casting is the process of converting a variable to another
+
+name = "Noriel"
+age = 22
+grade = 1.15
+is_student = False
+
+print(type(name))
+
+# int()
+
+grade = int(grade)
+
+print(f"{grade }")
+
+# float()
+
+age = float(age)
+
+print(age)
+
+# str()
+
+age = str(age)
+
+print(age)
+
+# bool()
+
+name = bool(name)
+
+print(name) # Prints False if the value of string is ""
+
